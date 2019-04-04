183f5e565b015ccb052f5fa0b20da738001f54c2
Khim22 <khimyang22@gmail.com>
2019-04-04 23:55:52 +0800

    (setup): completed setup of getting permissions to google map api

--- app/src/main/java/com/example/mydomain/pathtracer/MapsActivity.java
+++ app/src/main/java/com/example/mydomain/pathtracer/MapsActivity.java
@@ -1,7 +1,14 @@
 package com.example.mydomain.pathtracer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,20 +21,71 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private static final String TAG = "MapsActivity";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOC_PERMISSION_ACCESS_CODE = 1234;
+
+    private boolean mLocationPermissionGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        getLocationPermission();
     }
 
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: getting location permissions..");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionGranted = true;
+            }
+            else{
+                ActivityCompat.requestPermissions(this, permissions,
+                        LOC_PERMISSION_ACCESS_CODE);
+            }
+        }
+        else{
+            ActivityCompat.requestPermissions(this, permissions,
+                    LOC_PERMISSION_ACCESS_CODE);
+        }
+    }
+
+    private void initMap(){
+        Log.d(TAG, "initMap: initializing map...");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapsActivity.this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: executing...");
+        mLocationPermissionGranted = false;
+        switch(requestCode){
+            case LOC_PERMISSION_ACCESS_CODE:
+                if(grantResults.length>0){
+                    for(int i=0; i< grantResults.length; i++){
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionGranted = false;
+                            Log.d(TAG, "onRequestPermissionsResult: Permission failed");
+                            return;
+                        }
+                    }
+                    mLocationPermissionGranted = true;
+                    Log.d(TAG, "onRequestPermissionsResult: Permission granted");
+                    initMap();
+                }
+
+        }
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady: map ready");
+        Toast.makeText(this, "Map Ready" , Toast.LENGTH_SHORT).show();
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
