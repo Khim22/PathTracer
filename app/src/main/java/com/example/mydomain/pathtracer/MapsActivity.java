80aa13a8d3308d17c8538f56456bb6244ef5dc98
Khim22 <khimyang22@gmail.com>
2019-04-06 22:03:05 +0800

    (feat): updated UI components

--- app/src/main/java/com/example/mydomain/pathtracer/MapsActivity.java
+++ app/src/main/java/com/example/mydomain/pathtracer/MapsActivity.java
@@ -1,21 +1,39 @@
 package com.example.mydomain.pathtracer;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -24,8 +42,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOC_PERMISSION_ACCESS_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 16f;
 
     private boolean mLocationPermissionGranted = false;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,27 +53,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         getLocationPermission();
+
+        final Button btn  = findViewById(R.id.overlay_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView tv = findViewById(R.id.overlay);
+                tv.setVisibility(View.INVISIBLE);
+                btn.setVisibility(View.INVISIBLE);
+            }
+        });
+
     }
 
-    private void getLocationPermission(){
+    private void getLocationPermission() {
         Log.d(TAG, "getLocationPermission: getting location permissions..");
         String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
-        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 mLocationPermissionGranted = true;
-            }
-            else{
+                initMap();
+            } else {
                 ActivityCompat.requestPermissions(this, permissions,
                         LOC_PERMISSION_ACCESS_CODE);
             }
-        }
-        else{
+        } else {
             ActivityCompat.requestPermissions(this, permissions,
                     LOC_PERMISSION_ACCESS_CODE);
         }
     }
 
-    private void initMap(){
+    private void getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation: getting current location");
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+            if (mLocationPermissionGranted) {
+                Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "onComplete: Location Found");
+                            Location currentLocation = (Location) task.getResult();
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+                            double currentLat = currentLocation.getLatitude();
+                            double currentLong = currentLocation.getLongitude();
+
+
+                            //start of polyline trial
+                            List<LatLng> latLngs = new ArrayList<LatLng>();
+                            latLngs.add(
+                                    new LatLng(currentLat,currentLong)
+                            );
+                            latLngs.add(
+                                    new LatLng(currentLat-0.1,currentLong-0.1)
+                            );
+                            latLngs.add(
+                                    new LatLng(currentLat + 0.1,currentLong-0.1)
+                            );
+                            Polyline polyline = mMap.addPolyline(new PolylineOptions().addAll(latLngs));
+                            polyline.setColor(Color.BLUE);
+                            polyline.setClickable(true);
+
+                            Polygon polygon = mMap.addPolygon(new PolygonOptions().addAll(latLngs));
+                            polygon.setStrokeColor(Color.RED);
+                            polygon.setFillColor(ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary));
+                            polygon.setStrokeWidth(10);
+                            polygon.setGeodesic(true);
+                            //end polyline trial
+                        } else {
+                            Log.d(TAG, "onComplete: current location NULL");
+                            Toast.makeText(MapsActivity.this, "Unable to get current location of device", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: SecurityException" + e.getMessage());
+        }
+
+    }
+
+    private void initMap() {
         Log.d(TAG, "initMap: initializing map...");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -64,11 +145,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         Log.d(TAG, "onRequestPermissionsResult: executing...");
         mLocationPermissionGranted = false;
-        switch(requestCode){
+        switch (requestCode) {
             case LOC_PERMISSION_ACCESS_CODE:
-                if(grantResults.length>0){
-                    for(int i=0; i< grantResults.length; i++){
-                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                             mLocationPermissionGranted = false;
                             Log.d(TAG, "onRequestPermissionsResult: Permission failed");
                             return;
@@ -78,19 +159,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     Log.d(TAG, "onRequestPermissionsResult: Permission granted");
                     initMap();
                 }
-
         }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Log.d(TAG, "onMapReady: map ready");
-        Toast.makeText(this, "Map Ready" , Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Map Ready", Toast.LENGTH_SHORT).show();
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (mLocationPermissionGranted) {
+            getDeviceLocation();
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            mMap.getUiSettings().setCompassEnabled(true);
+        }
+    }
+
+    private void moveCamera(LatLng latlng, float zoom){
+        Log.d(TAG, "moveCamera: moving camera to lat:"+ latlng.latitude +", lng:" + latlng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoom));
     }
 }
